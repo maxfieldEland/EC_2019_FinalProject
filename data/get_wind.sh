427f331969d2f662d776277234a3f31558bfa0e4
Maxfield Green <mgreen13@uvm.edu>
2019-10-28 10:55:51 -0400

    added lit review in writing/gecco_format_docs, added topography data and updata weather data in the data folder. Redesigned folder layout for clarity and organization

---
+++ data/get_wind.sh
@@ -0,0 +1,1 @@
+for year in `seq 2016 2016`;do for month in `seq 5 8`;do wget --content-disposition "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=27216&Year=${year}&Month=${month}&Day=14&timeframe=1&submit= Download+Data" ;done;done
